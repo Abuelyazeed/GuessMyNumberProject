dcb6fa6c5af294e4e0ec9773322a80f0a847898f
Abuelyazeed <abuelyazeed175@gmail.com>
2022-06-15 01:12:50 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = '🚫 No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too High';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too Low';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+});
